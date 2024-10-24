81bfda3debc5d834c39140b6bb091f9bafa579a3
malieo1 <zahmoul.malek@etudiant-fst.utm.tn>
2024-10-24 17:34:51 +0100

    h2

--- src/test/java/tn/esprit/spring/SkierServiceImplTest.java
+++ src/test/java/tn/esprit/spring/SkierServiceImplTest.java
@@ -7,6 +7,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 import tn.esprit.spring.entities.Skier;
 import tn.esprit.spring.repositories.ISkierRepository;
 import tn.esprit.spring.services.SkierServicesImpl;
@@ -22,6 +23,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
 class SkierServiceImplTest {
 
     @InjectMocks
