718480715a88fb99bf377394bcfc498a9263fb4d
mathlouthiyoussef <mohamedyoussef.mathlouthi@esprit.tn>
2024-10-30 23:43:08 +0100

    modification 6

--- src/test/java/tn/esprit/spring/CourseRestControllerTest.java
+++ src/test/java/tn/esprit/spring/CourseRestControllerTest.java
@@ -11,11 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import tn.esprit.spring.controllers.CourseRestController;
 import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.entities.Support;
+import tn.esprit.spring.entities.TypeCourse;
 import tn.esprit.spring.services.ICourseServices;
 
 import java.util.Collections;
 
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CourseRestController.class)
@@ -29,7 +32,7 @@ class CourseRestControllerTest {
 
     @Test
     void testGetAllCourses() throws Exception {
-        Course course = new Course(1L, 1, null, null, 100.0f, 10, null);
+        Course course = new Course(1L, 1, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, 100.0f, 10, null);
         when(courseServices.retrieveAllCourses()).thenReturn(Collections.singletonList(course));
 
         mockMvc.perform(MockMvcRequestBuilders.get("/course/all"))
@@ -40,20 +43,24 @@ class CourseRestControllerTest {
 
     @Test
     void testAddCourse() throws Exception {
-        Course course = new Course(1L, 1, null, null, 100.0f, 10, null);
-        when(courseServices.addCourse(course)).thenReturn(course);
+        Course course = new Course(1L, 1, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, 100.0f, 10, null);
+        when(courseServices.addCourse(any(Course.class))).thenReturn(course);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/course/add")
                         .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"numCourse\":1,\"level\":1,\"price\":100.0,\"timeSlot\":10}"))
+                        .content("{\"level\":1,\"typeCourse\":\"SOME_TYPE\",\"support\":\"SOME_SUPPORT\",\"price\":100.0,\"timeSlot\":10}"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.numCourse").value(course.getNumCourse()))
-                .andExpect(jsonPath("$.level").value(course.getLevel()));
+                .andExpect(jsonPath("$.level").value(course.getLevel()))
+                .andExpect(jsonPath("$.typeCourse").value(course.getTypeCourse().toString()))
+                .andExpect(jsonPath("$.support").value(course.getSupport().toString()))
+                .andExpect(jsonPath("$.price").value(course.getPrice()))
+                .andExpect(jsonPath("$.timeSlot").value(course.getTimeSlot()));
     }
 
     @Test
     void testGetCourseById() throws Exception {
-        Course course = new Course(1L, 1, null, null, 100.0f, 10, null);
+        Course course = new Course(1L, 1, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, 100.0f, 10, null);
         when(courseServices.retrieveCourse(1L)).thenReturn(course);
 
         mockMvc.perform(MockMvcRequestBuilders.get("/course/get/1"))
