462d7119660d9f9297e83ea78dd29774b7c0d379
rezgui amine <mohammedamine.rezgui@etudiant-fst.utm.tn>
2024-11-01 14:25:06 +0100

    finaaal

--- src/test/java/tn/esprit/spring/SubsriptionTest.java
+++ src/test/java/tn/esprit/spring/SubsriptionTest.java
@@ -1,12 +1,11 @@
 package tn.esprit.spring;
 
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.spring.entities.Subscription;
 import tn.esprit.spring.entities.TypeSubscription;
 import tn.esprit.spring.repositories.ISubscriptionRepository;
@@ -14,26 +13,18 @@ import tn.esprit.spring.repositories.ISubscriptionRepository;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@ActiveProfiles("test")  // Points to an application-test.properties file for H2 configuration
 public class SubsriptionTest {
 
-
-
-    @Mock
+    @Autowired
     private ISubscriptionRepository subscriptionRepository;
 
-    @InjectMocks
-    private Subscription subscription;
-
-    @BeforeEach
-    void setUp() {
-        subscription = new Subscription(1L, LocalDate.now(), LocalDate.now().plusMonths(1), 29.99F, TypeSubscription.MONTHLY);
-    }
     @Test
     public void testSubscriptionCreation() {
-        when(subscriptionRepository.save(subscription)).thenReturn(subscription);
+        Subscription subscription = new Subscription(1L, LocalDate.now(), LocalDate.now().plusMonths(1), 29.99F, TypeSubscription.MONTHLY);
 
         Subscription savedSubscription = subscriptionRepository.save(subscription);
 
