6b458930bf266f07d405328550211ae4c6172f01
mathlouthiyoussef <mohamedyoussef.mathlouthi@esprit.tn>
2024-11-01 21:21:36 +0100

    modification 7

--- src/test/java/tn/esprit/spring/CourseRestControllerTest.java
+++ src/test/java/tn/esprit/spring/CourseRestControllerTest.java
@@ -40,24 +40,24 @@ class CourseRestControllerTest {
                 .andExpect(jsonPath("$[0].numCourse").value(course.getNumCourse()))
                 .andExpect(jsonPath("$[0].level").value(course.getLevel()));
     }
-//
-//    @Test
-//    void testAddCourse() throws Exception {
-//        Course course = new Course(1L, 1, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, 100.0f, 10, null);
-//        when(courseServices.addCourse(any(Course.class))).thenReturn(course);
-//
-//        mockMvc.perform(MockMvcRequestBuilders.post("/course/add")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content("{\"level\":1,\"typeCourse\":\"SOME_TYPE\",\"support\":\"SOME_SUPPORT\",\"price\":100.0,\"timeSlot\":10}"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.numCourse").value(course.getNumCourse()))
-//                .andExpect(jsonPath("$.level").value(course.getLevel()))
-//                .andExpect(jsonPath("$.typeCourse").value(course.getTypeCourse().toString()))
-//                .andExpect(jsonPath("$.support").value(course.getSupport().toString()))
-//                .andExpect(jsonPath("$.price").value(course.getPrice()))
-//                .andExpect(jsonPath("$.timeSlot").value(course.getTimeSlot()));
-//    }
-//
+
+    @Test
+    void testAddCourse() throws Exception {
+        Course course = new Course(1L, 1, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, 100.0f, 10, null);
+        when(courseServices.addCourse(any(Course.class))).thenReturn(course);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/course/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"level\":1,\"typeCourse\":\"SOME_TYPE\",\"support\":\"SOME_SUPPORT\",\"price\":100.0,\"timeSlot\":10}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.numCourse").value(course.getNumCourse()))
+                .andExpect(jsonPath("$.level").value(course.getLevel()))
+                .andExpect(jsonPath("$.typeCourse").value(course.getTypeCourse().toString()))
+                .andExpect(jsonPath("$.support").value(course.getSupport().toString()))
+                .andExpect(jsonPath("$.price").value(course.getPrice()))
+                .andExpect(jsonPath("$.timeSlot").value(course.getTimeSlot()));
+    }
+
     @Test
     void testGetCourseById() throws Exception {
         Course course = new Course(1L, 1, TypeCourse.COLLECTIVE_CHILDREN, Support.SKI, 100.0f, 10, null);
