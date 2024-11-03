8c2211ee1244e55422e6dae259678b61df194c4f
rezgui amine <mohammedamine.rezgui@etudiant-fst.utm.tn>
2024-11-03 13:30:13 +0100

    fixing docker compose

--- src/test/java/tn/esprit/spring/SubsriptionTest.java
+++ src/test/java/tn/esprit/spring/SubsriptionTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
 import tn.esprit.spring.entities.Subscription;
 import tn.esprit.spring.entities.TypeSubscription;
 import tn.esprit.spring.repositories.ISubscriptionRepository;
@@ -16,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@ActiveProfiles
 public class SubsriptionTest {
 
     @Mock
