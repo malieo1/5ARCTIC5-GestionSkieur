c88f9b532571ae4060c9f5a434164f2140368a05
rezgui amine <mohammedamine.rezgui@etudiant-fst.utm.tn>
2024-11-07 11:55:11 +0100

    amin

--- src/test/java/tn/esprit/spring/SubscriptionServiceTest.java
+++ src/test/java/tn/esprit/spring/SubscriptionServiceTest.java
@@ -1,16 +1,13 @@
 package tn.esprit.spring;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.spring.entities.Subscription;
 import tn.esprit.spring.entities.TypeSubscription;
 import tn.esprit.spring.repositories.ISubscriptionRepository;
